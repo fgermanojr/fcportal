a7f9bf53de8d0eb31a91988e4aa623a59c5867d4
Frank Germano <frank@jackrabbitsystems.com>
2017-10-07 11:34:24 -0700

    cable not yet working

--- app/assets/javascripts/channels/notification.coffee
+++ app/assets/javascripts/channels/notification.coffee
@@ -1,11 +1,17 @@
-App.notification = App.cable.subscriptions.create "NotificationChannel",
+// put this in function in status object to set up this channel. then this
+// will refer to that components updateStatus
+App.notification = App.cable.subscriptions.create "NotificationChannel", {
   connected: ->
     # Called when the subscription is ready for use on the server
 
   disconnected: ->
     # Called when the subscription has been terminated by the server
 
-  received: (data) ->
+  received: (data) -> {
     # Called when there is incoming data on the websocket for this channel
-       new Notification data['status'];
-    # TODO change state in status object
+       this.updateStatus(data.status)
+   }
+
+   updateStatus: this.updateStatus
+
+};
--- app/javascript/bundles/Portal/Components/Status.jsx
+++ app/javascript/bundles/Portal/Components/Status.jsx
@@ -11,10 +11,38 @@ export default class Status extends React.Component {
     var s = myStyles.Styles;
     return (
       <div id="status">
-        STATUS{this.props.notification}
+        STATUS {this.props.notification}
       </div>
     );
-
-
   }
+
+  componentDidMount() {
+    this.setupSubscription();
+  }
+
+
+  setupSubscription() {
+
+    // App.notification = App.cable.subscriptions.create("NotificationChannel", {
+    //   connected: function() {
+    //     // Called when the subscription is ready for use on the server
+    //   },
+
+    //   disconnected: function() {
+    //     // Called when the subscription has been terminated by the server
+    //   },
+
+    //   received: function(data) {
+    //     // Called when there is incoming data on the websocket for this channel
+    //     this.updateStatus(data.status)
+    //   }
+    // })
+  }
+
+  // updateStatus: this.updateStatus
+
+
+  updateStatus(status) {
+    this.setState({status: status});
+  };
 }
