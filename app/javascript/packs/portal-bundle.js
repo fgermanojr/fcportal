333f4814d2b92c29d520e3f280f145642733fbb2
Frank Germano <frank@jackrabbitsystems.com>
2017-11-22 11:51:59 -0800

    fgj changed import Portal path in portal-bundle.js

--- app/javascript/packs/portal-bundle.js
+++ app/javascript/packs/portal-bundle.js
@@ -1,6 +1,6 @@
 import ReactOnRails from 'react-on-rails';
 
-import Portal from '../bundles/Portal/components/Portal';
+import Portal from './Portal/components/Portal';
 
 // This is how react_on_rails can see the Portal in the browser.
 ReactOnRails.register({
