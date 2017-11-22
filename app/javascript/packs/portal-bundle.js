fbb801997af09769ddae1c7cf8019341c41880fc
Frank Germano <frank@jackrabbitsystems.com>
2017-11-22 14:24:43 -0800

    fgj not coming up locally, pack_tag is ''

--- app/javascript/packs/portal-bundle.js
+++ app/javascript/packs/portal-bundle.js
@@ -1,6 +1,6 @@
 import ReactOnRails from 'react-on-rails';
 
-import Portal from './Portal/components/Portal';
+import Portal from '../components/Portal';
 
 // This is how react_on_rails can see the Portal in the browser.
 ReactOnRails.register({
