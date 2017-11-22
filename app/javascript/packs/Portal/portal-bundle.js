a0578a682d7c1f64699d76faae6b4f637628c17b
Frank Germano <frank@jackrabbitsystems.com>
2017-11-22 13:24:18 -0800

    fgj put the bundles dir back as components, changed webpacker config to pull

--- app/javascript/packs/Portal/portal-bundle.js
+++
@@ -1,8 +0,0 @@
-import ReactOnRails from 'react-on-rails';
-
-import Portal from '../bundles/Portal/components/Portal';
-
-// This is how react_on_rails can see the Portal in the browser.
-ReactOnRails.register({
-  Portal
-});
