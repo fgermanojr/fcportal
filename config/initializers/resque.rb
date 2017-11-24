ecf2af3166f53c1daf35448532778b570e559538
Frank Germano <frank@jackrabbitsystems.com>
2017-11-24 09:53:25 -0800

    fgj tweak to redis config for rescue

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,1 +1,11 @@
 Resque.logger.formatter = Resque::VeryVerboseFormatter.new
+
+# if Rails.env.production?  # TBD: remove
+#   uri = URI.parse ENV["REDISCLOUD_URL"]
+#   Resque.redis = Redis.new(host: uri.host, port: uri.port,
+#                            password: uri.password)
+# else
+#   Resque.redis = "localhost:6379"
+# end
+
+Resque.redis = Rails.env.production? ? $redis : 'localhost:6379'
