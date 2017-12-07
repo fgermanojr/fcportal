425dabc03d84436b81bb7f73cc139feeccd63567
Frank Germano <frank@jackrabbitsystems.com>
2017-12-07 14:13:38 -0700

    fgj removed manual/images components except introduction. will be served
    from rails into an iframe (or another component on the portal entry page)

--- app/javascript/components/Doc.jsx
+++ app/javascript/components/Doc.jsx
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import * as myStyles from './Styles.js';
-// import map from './images/OlympicNationalParkMap.jpg'; //import image, rel path
+// import map from './images/OlympicNationalParkMap.jpg'; //import image, rel path, tested, works
 import Introduction from './manual/introduction.jsx'
 //import Chap1 from './manual/Man/chap1/FCMANC1.jsx'
-import Chap2 from './manual/Man/chap2/FCMANC2.jsx'
-import Chap3 from './manual/Man/chap3/FCMANC3.jsx'
-import Chap4 from './manual/Man/chap4/FCMANC4.jsx'
+// import Chap2 from './manual/Man/chap2/FCMANC2.jsx'
+// import Chap3 from './manual/Man/chap3/FCMANC3.jsx'
+// import Chap4 from './manual/Man/chap4/FCMANC4.jsx'
 // import Scroll from 'react-scroll'; // Imports all Mixins    //NOT WORKING YET  does it work on a div ???
 
 export default class Doc extends React.Component {
@@ -17,7 +17,7 @@ export default class Doc extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {current_section: 'chapter4'}
+    this.state = {current_section: 'Introduction'}
   }
 
   render() {
@@ -29,18 +29,18 @@ export default class Doc extends React.Component {
       case 'introduction':
         view_object = <Introduction />;
         break;
-      case 'chapter1':
-        view_object = <Chap1 />;
-        break;
-      case 'chapter2':
-        view_object = <Chap2 />;
-        break;
-      case 'chapter3':
-        view_object = <Chap3 />;
-        break;
-      case 'chapter4':
-        view_object = <Chap4 />;
-        break;
+      // case 'chapter1':
+      //   view_object = <Chap1 />;
+      //   break;
+      // case 'chapter2':
+      //   view_object = <Chap2 />;
+      //   break;
+      // case 'chapter3':
+      //   view_object = <Chap3 />;
+      //   break;
+      // case 'chapter4':
+      //   view_object = <Chap4 />;
+      //   break;
       default:
         view_object = <NotYetImplemented />;
         break;
